ae4789682cbedbbaccf4044e2d14b86190febcfb
Krushna <sonawanekrishna9130@gmail.com>
2024-12-30 17:24:44 +0530

    Book Store Application MERN stack project

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5001;
+
+export const mongoDBURL = 'mongodb+srv://root:Krish%409923@book-store-mern.dffqu.mongodb.net/book-collections?retryWrites=true&w=majority&appName=Book-store-MERN'
