0f319f325079a7350b6ed354b4c7c83b3cb02848
Krushna <sonawanekrishna9130@gmail.com>
2025-01-02 21:13:56 +0530

    Update frontend code for integrating with backend

--- backend/index.js
+++ backend/index.js
@@ -8,16 +8,16 @@ import cors from "cors";
 const app = express();
 
 // Middleware to parse JSON
-// app.use(express.json());
+app.use(express.json());
 
 // Middleware for handling CORS POLICY
 // Option 1: Allow All Origins with Default cors(*)
 app.use(cors());
 
 // Option 2: Allow Custom Origins
-/* app.use(
+/*app.use(
     cors({
-        origin: 'http://localhost:3000',
+        origin: 'http://localhost:5002',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type'],
     })
