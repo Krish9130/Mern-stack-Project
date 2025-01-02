dd323334a48c687f6805f0e6df2b80bc8fba8864
Krushna Sonawane <105139116+Krish9130@users.noreply.github.com>
2025-01-03 00:46:03 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5001;
 
-export const mongoDBURL = 'mongodb+srv://root:Krish%409923@book-store-mern.dffqu.mongodb.net/book-collections?retryWrites=true&w=majority&appName=Book-store-MERN'
-// export const mongoDBURL = 'mongodb+srv://root:Krish%409923@book-store-mern.dffqu.mongodb.net/book-collections?retryWrites=true&w=majority&appName=Book-store-MERN'
+export const mongoDBURL = 'mongodb+srv://root:<password>@book-store-mern.dffqu.mongodb.net/book-collections?retryWrites=true&w=majority&appName=Book-store-MERN'
+// export const mongoDBURL = 'mongodb+srv://root:<password>@book-store-mern.dffqu.mongodb.net/book-collections?retryWrites=true&w=majority&appName=Book-store-MERN'
